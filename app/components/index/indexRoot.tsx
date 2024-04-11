b20be23c8a3be670388944a9a4cb4b8c3e4ff4fe
CordobaBenjamin <cordobabenjamin37@gmail.com>
2024-04-11 00:47:49 -0300

    first commit

---
+++ app/components/index/indexRoot.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React, { Fragment, useState } from 'react';
+// import Navbar from './index components/navbar';
+// import Main_tittle from './index components/main_tittle';
+
+const IndexRoot: React.FC = () => {
+  return (
+    <>
+    {/* <Navbar/>
+    <Main_tittle/> */}
+    </>
+  )
+};
+
+export default IndexRoot;
---
+++ app/pages/index/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import IndexLayout from "@/app/components/index/indexLayout";
+
+const Page = () => {
+  return (
+   <>
+   <IndexLayout>
+    {null}
+   </IndexLayout>
+   </>
+  )
+}
+
+export default Page;
